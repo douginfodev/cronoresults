8d33f28b469bbade67c3e81e32c5a6f5035310b1
= <=>
2023-10-14 23:01:22 -0300

    Created Index.blade.php

---
+++ resources/views/index.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt_BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Crono Results</title>
+</head>
+
+<body>
+    <content-wrapper>
+        <header-main>
+
+        </header-main>
+        <content-main>
+
+        </content-main>
+        <footer-main>
+            
+        </footer-main>
+    </content-wrapper>
+</body>
+
+</html>
